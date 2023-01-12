8c0e5d4e9ea3f0469134dc293282e5a79bf9856f
yao-tx <7468923+yao-tx@users.noreply.github.com>
2023-01-13 02:30:32 +0800

    testing metamask connection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import * as React from "react";
+import React, { useEffect } from "react";
 import { ethers } from "ethers";
 import "./App.css";
+
+const getEthereumObject = () => window.ethereum;
 
 function App() {
   const wave = () => {
 
   }
+
+  useEffect(() => {
+    const ethereum = getEthereumObject();
+    if (!ethereum) {
+      console.log("Make sure you have metamask!");
+    } else {
+      console.log("We have the ethereum object", ethereum);
+    }
+  }, []);
 
   return (
     <div className="mainContainer">
@@ -14,10 +25,11 @@ function App() {
           👋 Hey there!
         </div>
         <div className="bio">
-          I'm TX, a full-stack web dev from Singapore learning about Web3.
+          I'm TX, a full-stack web dev from Singapore learning about Web3.<br />
+          Connect your Ethereum wallet and wave at me!
         </div>
         <button className="waveButton" onClick={wave}>
-          Wave at Me!
+          Wave at Me
         </button>
       </div>
     </div>
