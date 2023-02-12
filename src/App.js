9b56e3addeee4aa061fd12590620e84084bbe918
yao-tx <yao_tengxiong@hotmail.com>
2023-02-13 02:57:48 +0800

    fix stuff

--- src/App.js
+++ src/App.js
@@ -122,6 +122,36 @@ function App() {
     checkIfWalletIsConnected();
   }, []);
 
+  useEffect(() => {
+    let wavePortalContract;
+
+    const onNewWave = (from, timestamp, message) => {
+      console.log("NewWave", from, timestamp, message);
+      setAllWaves(prevState => [
+        ...prevState,
+        {
+          address: from,
+          timestamp: new Date(timestamp * 1000),
+          message: message,
+        },
+      ]);
+    };
+
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+      wavePortalContract.on("NewWave", onNewWave);
+    }
+
+    return () => {
+      if (wavePortalContract) {
+        wavePortalContract.off("NewWave", onNewWave);
+      }
+    };
+  }, []);
+
   let buttonText = "Wave at Me";
 
   if (isLoading) {
